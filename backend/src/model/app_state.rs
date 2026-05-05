5159e9766869af34b91d35987c3f5df1a29050f2
191-iota <101976629+191-iota@users.noreply.github.com>
2026-05-05 14:19:10 +0200

    feat: add overlay model and live-state scaffolding

--- backend/src/model/app_state.rs
+++ backend/src/model/app_state.rs
@@ -1,7 +1,14 @@
 use std::path::PathBuf;
 
+use dashmap::DashMap;
+use uuid::Uuid;
+
+use super::overlay::ProjectLiveState;
+
 // So we do not need to re-init everytime we use it in handlers
 #[derive(Clone)]
 pub struct AppState {
+    // Key: project id; Value: live state
+    pub repo_states: DashMap<Uuid, ProjectLiveState>,
     pub repo_loc: PathBuf,
 }
--- backend/src/model/mod.rs
+++ backend/src/model/mod.rs
@@ -1,2 +1,3 @@
 pub mod app_state;
+pub mod overlay;
 pub mod project;
