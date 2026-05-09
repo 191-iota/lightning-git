8ddae9f7df762dee7ad4cba104962aaa0ad84a4b
191-iota <101976629+191-iota@users.noreply.github.com>
2026-05-09 14:16:26 +0200

    feat: placeholder extension code

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,26 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hf-lightning-git-vsc" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hf-lightning-git-vsc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hf-lightning-git-vsc!');
-	});
-
-	context.subscriptions.push(disposable);
+  // TODO: auth, views, and backend clients.
+  context.subscriptions.push(
+    vscode.commands.registerCommand("lightningGit.signIn", async () => {
+      void vscode.window.showInformationMessage("Lightning Git: not wired up yet.");
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // no-op
+}
